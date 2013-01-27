04670bde4c561c59d797aec91d13b45ab27155b4
Ted Lawless <lawlesst@gmail.com>
2013-01-27 16:13:16 -0500

    URL encode test urls.

--- test/account.py
+++ test/account.py
@@ -26,6 +26,7 @@ class AccountTest(unittest.TestCase):
     def test_submit_key(self):
        ill = self.ill
        ill.login()
+       #Url encoded
        openurl = "rft_val_fmt=info%3Aofi%2Ffmt%3Akev%3Amtx%3Ajournal&rft.spage=538&rft.issue=5&rft.date=2010-02-11&rft.volume=16&url_ver=Z39.88-2004&rft.atitle=Targeting+%CE%B17+Nicotinic+Acetylcholine+Receptors+in+the+Treatment+of+Schizophrenia.&rft.jtitle=Current+pharmaceutical+design&rft.issn=1381-6128&rft.genre=article"
        submit_key = ill.get_request_key(openurl)
        self.assertEqual(submit_key['ILLiadForm'],
@@ -37,7 +38,8 @@ class AccountTest(unittest.TestCase):
     def test_book(self):
       ill = self.ill
       ill.login()
-      openurl = u"sid=FirstSearch:WorldCat&genre=book&isbn=9780231122375&title=Mahatma Gandhi : nonviolent power in action&date=2000&aulast=Dalton&aufirst=Dennis&id=doi:&pid=<accession number>46863142</accession number><fssessid>0</fssessid>&url_ver=Z39.88-2004&rfr_id=info:sid/firstsearch.oclc.org:WorldCat&rft_val_fmt=info:ofi/fmt:kev:mtx:book&req_dat=<sessionid>0</sessionid>&rfe_dat=<accessionnumber>46863142</accessionnumber>&rft_id=info:oclcnum/46863142&rft_id=urn:ISBN:9780231122375&rft.aulast=Dalton&rft.aufirst=Dennis&rft.btitle=Mahatma Gandhi : nonviolent power in action&rft.date=2000&rft.isbn=9780231122375&rft.place=New York&rft.pub=Columbia University Press&rft.genre=book"
+      #Url encoded
+      openurl = u"sid=FirstSearch:WorldCat&genre=book&isbn=9780231122375&title=Mahatma%20Gandhi%20%3A%20nonviolent%20power%20in%20action&date=2000&rft.genre=book"
       submit_key = ill.get_request_key(openurl)
       self.assertEqual(submit_key['ILLiadForm'], 'LoanRequest')
       self.assertEqual(submit_key['LoanTitle'], 'Mahatma Gandhi : nonviolent power in action')

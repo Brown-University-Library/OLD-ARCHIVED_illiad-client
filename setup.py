8f8aa6ffd4869e6dd1fb21702d4adb6ccd3743cb
birkin <birkin.diana@gmail.com>
2015-10-21 14:44:35 -0400

    enables pip-install.

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ from setuptools import setup, find_packages
 setup(name='illiad',
     version='0.3',
     packages=find_packages(),
-    package_data={'illiad': ['test/data/*.*']},
     install_requires=[
         'requests',
         'beautifulsoup4',
